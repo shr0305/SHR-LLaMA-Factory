0527294c3f3251abbc535d0a1b51a20e17e15352
施氏食狮史 <shr1549056828@126.com>
2025-04-19 05:31:20 +0000

    提交了新的数据集

--- src/api.py
+++ src/api.py
@@ -24,7 +24,7 @@ def main():
     chat_model = ChatModel()
     app = create_app(chat_model)
     api_host = os.getenv("API_HOST", "0.0.0.0")
-    api_port = int(os.getenv("API_PORT", "8000"))
+    api_port = int(os.getenv("API_PORT", "5500"))
     print(f"Visit http://localhost:{api_port}/docs for API document.")
     uvicorn.run(app, host=api_host, port=api_port)
 
--- src/llamafactory/api/app.py
+++ src/llamafactory/api/app.py
@@ -128,6 +128,6 @@ def run_api() -> None:
     chat_model = ChatModel()
     app = create_app(chat_model)
     api_host = os.getenv("API_HOST", "0.0.0.0")
-    api_port = int(os.getenv("API_PORT", "8000"))
+    api_port = int(os.getenv("API_PORT", "5500"))
     print(f"Visit http://localhost:{api_port}/docs for API document.")
     uvicorn.run(app, host=api_host, port=api_port)
